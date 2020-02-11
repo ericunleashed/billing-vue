61221cd979d50f77c7fe483fb1b1b58fc478ec78
ericunleashed <eric@unleashedsoftware.com>
2020-02-12 09:08:54 +1300

    set up subscription plans info and styles

--- src/App.vue
+++ src/App.vue
@@ -17,15 +17,36 @@ export default {
       plans: [
         {
           name: 'Medium',
-          price: 49
+          price: 49,
+          currency: '$',
+          features: [
+            {feature: '3 Users', available: true},
+            {feature: '125,000 API calls', available: true},
+            {feature: 'Unleashed University', available: false},
+            {feature: 'Premium Support', available: false}
+          ]
         },
         {
           name: 'Large',
-          price: 89
+          price: 89,
+          currency: '$',
+          features: [
+            {feature: '8 Users', available: true},
+            {feature: '250,000 API calls', available: true},
+            {feature: 'Unleashed University', available: true},
+            {feature: 'Premium Support', available: false}
+          ]
         },
         {
           name: 'Large Plus',
-          price: 129
+          price: 129,
+          currency: '$',
+          features: [
+            {feature: '20 Users', available: true},
+            {feature: '500,000 API calls', available: true},
+            {feature: 'Unleashed University', available: true},
+            {feature: 'Premium Support', available: true}
+          ]
         }
       ]
     }
@@ -46,5 +67,19 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.btn-default {
+  font-size: 16px;
+  font-weight: 500;
+  height: 38px;
+  padding: 0 30px;
+  background-color: #4a4a4a;
+  color: #ffffff;
+  border: none;
+  border-radius: 4px;
+  z-index: 1;
+  line-height: normal;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
 }
 </style>
--- src/components/Plan.vue
+++ src/components/Plan.vue
@@ -2,11 +2,14 @@
     <div class="plan-container">
         <div class="price-container">
             <h4>{{plan.name}}</h4>
-            <div class="price">{{plan.price}}</div>
+            <div class="price">{{plan.currency}}{{plan.price}}</div>
         </div>
-        <div class="description-container"></div>
+        <div class="description-container">
+            <div class="feature" v-for="feature in plan.features" v-bind:key="feature.feature" 
+            v-bind:class="{'unavailable':!feature.available}" v-bind:feature="feature">{{feature.feature}}</div>
+        </div>
         <div class="button-container">
-            <button class="btn-subscribe">Subscribe</button>
+            <button class="btn-default btn-subscribe">Subscribe</button>
         </div>
     </div>
 </template>
@@ -20,6 +23,9 @@ export default {
 
 <style lang="less" scoped>
 .plan-container {
+    flex: 1 1 30%;
+    min-width: 200px;
+    max-width: 320px;
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -27,19 +33,42 @@ export default {
     text-align: center;
     border: 1px solid lightgray;
     padding: 5px;
-    width: 200px;
-    min-height: 250px;
+    color: #666;
+    border-radius: 5px;
+    border: 1px solid #E1E7E9;
+    margin: 5px;
     .price-container {
         display: flex;
         flex-direction: column;
         align-items: center;
         justify-content: center;
         padding: 5px;
+        margin-top: 10px;
         .price {
             font-size: 40px;
         }
         h4 {
             margin-bottom: 5px;
+        }
+    }
+    .description-container {
+        margin: 20px auto;
+        .feature {
+            margin-bottom: 7px;
+            color: #FC4F4F;
+            &.unavailable {
+                color: #dddddd;
+                text-decoration: line-through;
+            }
+        }
+    }
+    .button-container {
+        margin-bottom: 10px;
+        .btn-subscribe {
+            background-color: #FC4F4F;
+            &:hover {
+                background-color: rgb(214, 54, 54);
+            }
         }
     }
 }
