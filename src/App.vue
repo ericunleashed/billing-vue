a12e8bdd1027c984e22daa1620b4073ea2fa8df5
Eric <zhangl127@myunitec.ac.nz>
2020-02-17 22:09:22 +1300

    Implement router

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,32 @@
 <template>
   <div id="app">
-    <Subscribe v-bind:plans="plans"/>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import Subscribe from './components/Subscribe.vue'
-
-export default {
-  name: 'App',
-  components: {
-    Subscribe
-  },
-  data() {
-    return {
-      plans: [
-        {
-          name: 'Medium',
-          price: 49
-        },
-        {
-          name: 'Large',
-          price: 89
-        },
-        {
-          name: 'Large Plus',
-          price: 129
-        }
-      ]
-    }
-  }
-}
-</script>
-
-<style>
-* {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-}
+<style lang="less">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <Subscribe v-bind:plans="plans"/>
+  </div>
+</template>
+
+<script>
+import Subscribe from '../components/Subscribe.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Subscribe
+  },
+  data() {
+    return {
+      plans: [
+        {
+          name: 'Medium',
+          price: 49
+        },
+        {
+          name: 'Large',
+          price: 89
+        },
+        {
+          name: 'Large Plus',
+          price: 129
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
